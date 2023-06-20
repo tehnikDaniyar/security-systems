393499a694db664c6960a68f7664bde7a53e1cb3
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-06-20 15:06:24 +0600

    connectioned script and styles for popups

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
 import { scrollHeader } from "./modules/scrollHeader.js";
 import { iconMenu } from "./modules/iconMenu.js";
+import { popups } from "./modules/popups.js";
 flsFunctions.isWebp();
 scrollHeader();
 iconMenu();
+popups();
